9052e3bcce35d5a1fea59cfce796c6a46dbc1fa3
Unn4m3DD <guerreiroapatacas@gmail.com>
2021-01-17 22:14:35 +0000

    first commit

---
+++ mapping.js
@@ -0,0 +1,15 @@
+document.onload(() => {
+  document.getElementsByClassName("video-stream html5-main-video")[0].focus()
+})
+document.addEventListener('keydown', (e) => {
+  if (e.key == "n") {
+    document.getElementsByClassName("video-stream html5-main-video")[0].focus()
+    document.getElementsByClassName("ytp-left-controls").click()
+    let ad = document.getElementsByClassName("ytp-ad-skip-button ytp-button")
+    for (let i = 0; i < ad.length; i++)
+      ad[i].click()
+    let banner = document.getElementsByClassName("ytp-ad-overlay-close-button")
+    for (let i = 0; i < banner.length; i++)
+      banner[i].clik()
+  }
+});
