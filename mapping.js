28ad51b25bda129c8715341218887f63e9287619
Unn4m3DD <guerreiroapatacas>
2021-01-20 11:32:02 +0000

    bug fix

--- mapping.js
+++ mapping.js
@@ -1,13 +1,15 @@
 setTimeout(() => { try { document.getElementsByClassName("video-stream html5-main-video")[0].focus() } catch { } }, 1000)
 document.addEventListener('keydown', (e) => {
   if (e.key == "n") {
-    document.getElementsByClassName("video-stream html5-main-video")[0].focus()
-    document.getElementsByClassName("ytp-left-controls").click()
-    let ad = document.getElementsByClassName("ytp-ad-skip-button ytp-button")
-    for (let i = 0; i < ad.length; i++)
-      ad[i].click()
-    let banner = document.getElementsByClassName("ytp-ad-overlay-close-button")
-    for (let i = 0; i < banner.length; i++)
-      banner[i].clik()
+    try {
+      document.getElementsByClassName("video-stream html5-main-video")[0].focus()
+      document.getElementsByClassName("ytp-left-controls").click()
+      let ad = document.getElementsByClassName("ytp-ad-skip-button ytp-button")
+      for (let i = 0; i < ad.length; i++)
+        ad[i].click()
+      let banner = document.getElementsByClassName("ytp-ad-overlay-close-button")
+      for (let i = 0; i < banner.length; i++)
+        banner[i].clik()
+    }catch { }
   }
 });
